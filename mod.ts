abd322fe5e53964090e46259c56ef650f817618a
mntcloud <40216914+mntcloud@users.noreply.github.com>
2021-10-03 12:46:50 +0300

    add marker function

--- mod.ts
+++ mod.ts
@@ -1,2 +1,10 @@
-export { Compiler } from './src/compiler.ts';
-export { Parser } from './src/parser.ts';
+import { Compiler } from './src/compiler.ts';
+import { Parser } from './src/parser.ts';
+
+export function marker(text: string): string {
+    const parser = new Parser(text);
+    parser.parse();
+
+    const compiler = new Compiler(parser);
+    return compiler.compile();
+}
